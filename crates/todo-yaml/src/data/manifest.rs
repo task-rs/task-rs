118bfb2931f7e14566fa345ea0c9f9f902e5d37a
khai96_ <hvksmr1996@gmail.com>
2020-06-05 17:22:11 +0700

    Create manifest

---
+++ crates/todo-yaml/src/data/manifest.rs
@@ -0,0 +1,9 @@
+use super::Task;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct Manifest {
+    pub manifest_version: String,
+    pub tasks: Vec<Task>,
+}
--- crates/todo-yaml/src/data/mod.rs
+++ crates/todo-yaml/src/data/mod.rs
@@ -1,5 +1,7 @@
+pub mod manifest;
 pub mod status;
 pub mod task;
 
+pub use manifest::*;
 pub use status::*;
 pub use task::*;
