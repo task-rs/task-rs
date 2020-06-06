1d0479d7dd1f72467864f4f11f326aa0044dcc59
khai96_ <hvksmr1996@gmail.com>
2020-06-06 19:44:56 +0700

    Make Config into a StructOpt

--- crates/todo-yaml/src/desktop/config.rs
+++ crates/todo-yaml/src/desktop/config.rs
@@ -1,7 +1,10 @@
 use serde::{Deserialize, Serialize};
+use structopt::StructOpt;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, StructOpt)]
 #[serde(rename_all = "kebab-case")]
+#[structopt(name = "todo-yaml")]
 pub struct Config {
+    #[structopt(long)]
     pub local_repo_location: String,
 }
