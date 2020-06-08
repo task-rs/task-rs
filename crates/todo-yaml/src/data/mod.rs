ef21f24ea62587b0da1200a270ce956dd40d1add
khai96_ <hvksmr1996@gmail.com>
2020-06-08 08:16:19 +0700

    Make re-exports more explicit

--- crates/todo-yaml/src/data/mod.rs
+++ crates/todo-yaml/src/data/mod.rs
@@ -3,7 +3,7 @@ pub mod status;
 pub mod tag;
 pub mod task;
 
-pub use manifest::*;
-pub use status::*;
-pub use tag::*;
-pub use task::*;
+pub use manifest::Manifest;
+pub use status::Status;
+pub use tag::Tag;
+pub use task::Task;
--- crates/todo-yaml/src/desktop/mod.rs
+++ crates/todo-yaml/src/desktop/mod.rs
@@ -2,6 +2,6 @@ pub mod app;
 pub mod args;
 pub mod config;
 
-pub use app::*;
-pub use args::*;
-pub use config::*;
+pub use app::App;
+pub use args::Args;
+pub use config::Config;
