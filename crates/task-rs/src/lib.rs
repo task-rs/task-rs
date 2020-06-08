2813165e05c276b631f1f477f854e47075d0a6ca
khai96_ <hvksmr1996@gmail.com>
2020-06-08 17:25:26 +0700

    Remove mvc

--- crates/task-rs/src/lib.rs
+++ crates/task-rs/src/lib.rs
@@ -1,6 +1,5 @@
 pub mod config;
 pub mod data;
 pub mod desktop;
-pub mod mvc;
 pub use serde;
 pub use structopt;
--- crates/task-rs/src/mvc/mod.rs
+++
@@ -1,8 +0,0 @@
-use super::config::Config;
-use super::data::Data;
-
-#[derive(Debug)]
-pub struct State {
-    pub manifest: Data,
-    pub config: Config,
-}
