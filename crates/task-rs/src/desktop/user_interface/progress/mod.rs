a541f8cfd436e0c05d36df588e61bd2c94f40412
khai96_ <hvksmr1996@gmail.com>
2020-06-09 16:40:21 +0700

    Set kebab-case to Progress

--- crates/task-rs/src/desktop/user_interface/progress/mod.rs
+++ crates/task-rs/src/desktop/user_interface/progress/mod.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 use smart_default::SmartDefault;
 
 #[derive(Debug, SmartDefault, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
 pub enum Progress {
     #[default]
     Operational,
