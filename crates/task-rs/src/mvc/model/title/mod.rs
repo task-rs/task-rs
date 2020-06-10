64e37739f0566c48ee57f4746ebcdaf261f6a51e
khai96_ <hvksmr1996@gmail.com>
2020-06-10 20:06:04 +0700

    Add Eq and PartialEq

--- crates/task-rs/src/mvc/model/title/mod.rs
+++ crates/task-rs/src/mvc/model/title/mod.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq)]
 #[serde(from = "String", into = "String")]
 pub struct Title(String);
 
