c2365dd3ff9dd8cf18ad7a8c68cc3985c2ef5317
khai96_ <hvksmr1996@gmail.com>
2020-06-10 20:09:47 +0700

    Make from_string generic

--- crates/task-rs/src/mvc/model/title/from_string.rs
+++ crates/task-rs/src/mvc/model/title/from_string.rs
@@ -1,12 +1,17 @@
 use super::Title;
 
-impl From<String> for Title {
-    fn from(text: String) -> Self {
-        Title(text)
+impl<Text: AsRef<str>> From<Text> for Title {
+    fn from(text: Text) -> Self {
+        Title(text.as_ref().to_owned())
     }
 }
 
 #[test]
 fn test_from_string() {
     assert_eq!(Title::from("foo".to_owned()), Title("foo".to_owned()));
+}
+
+#[test]
+fn test_from_str() {
+    assert_eq!(Title::from("foo"), Title("foo".to_owned()));
 }
