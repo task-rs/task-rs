0962d05337bce3d1d175c14bdcf81899509c1022
khai96_ <hvksmr1996@gmail.com>
2020-06-12 17:20:54 +0700

    Correct parameter names

--- crates/task-rs/src/mvc/controller/mod.rs
+++ crates/task-rs/src/mvc/controller/mod.rs
@@ -1,6 +1,6 @@
 use super::{Message, Model};
 use iced::Command;
 
-pub fn update(user_interface: &mut Model, message: Message) -> Command<Message> {
+pub fn update(model: &mut Model, message: Message) -> Command<Message> {
     Command::none()
 }
--- crates/task-rs/src/mvc/view/mod.rs
+++ crates/task-rs/src/mvc/view/mod.rs
@@ -1,7 +1,7 @@
 use super::{Message, Model};
 use iced::*;
 
-pub fn view(user_interface: &mut Model) -> Element<'_, Message> {
+pub fn view(model: &mut Model) -> Element<'_, Message> {
     Row::new()
         .push(Column::new().push(Text::new("TaskRs").color(Color::BLACK).size(40)))
         .into()
