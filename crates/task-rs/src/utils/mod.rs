403e7feb92222c15fda9c1244a44429791210103
khai96_ <hvksmr1996@gmail.com>
2020-06-13 17:18:23 +0700

    refactor: Use pipe_trait to make dot chain

--- crates/task-rs/src/utils/mod.rs
+++ crates/task-rs/src/utils/mod.rs
@@ -2,13 +2,14 @@ pub mod xdg;
 
 pub use xdg::{config_file, ui_state_file};
 
+use pipe_trait::*;
 use serde::de::DeserializeOwned;
 use std::{fs::File, path::PathBuf};
 
 pub fn deserialize_file<Output: DeserializeOwned>(filename: &PathBuf) -> Result<Output, String> {
-    let file = File::open(filename)
-        .map_err(|error| format!("Failed to load {}: {}", filename.to_string_lossy(), error))?;
-    let output = serde_yaml::from_reader(file)
-        .map_err(|error| format!("Failed to parse {}: {}", filename.to_string_lossy(), error))?;
-    Ok(output)
+    File::open(filename)
+        .map_err(|error| format!("Failed to load {}: {}", filename.to_string_lossy(), error))?
+        .pipe(serde_yaml::from_reader::<File, Output>)
+        .map_err(|error| format!("Failed to parse {}: {}", filename.to_string_lossy(), error))?
+        .pipe(Ok)
 }
