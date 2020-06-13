89b84209ed1b5827b8c3ed92e92256d33a7edc15
khai96_ <hvksmr1996@gmail.com>
2020-06-13 17:14:56 +0700

    Make error message more specific

--- crates/task-rs/src/utils/mod.rs
+++ crates/task-rs/src/utils/mod.rs
@@ -6,12 +6,10 @@ use core::fmt::Display;
 use serde::de::DeserializeOwned;
 use std::{fs::File, path::PathBuf};
 
-pub fn display(value: impl Display) -> String {
-    format!("{}", value)
-}
-
 pub fn deserialize_file<Output: DeserializeOwned>(filename: &PathBuf) -> Result<Output, String> {
-    let file = File::open(filename).map_err(display)?;
-    let model = serde_yaml::from_reader(file).map_err(display)?;
+    let file = File::open(filename)
+        .map_err(|error| format!("Failed to load {}: {}", filename.to_string_lossy(), error))?;
+    let model = serde_yaml::from_reader(file)
+        .map_err(|error| format!("Failed to parse {}: {}", filename.to_string_lossy(), error))?;
     Ok(model)
 }
