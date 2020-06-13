0c22ffe072ea4d633e8234a4551c1ddbd68d6dbb
khai96_ <hvksmr1996@gmail.com>
2020-06-13 14:20:40 +0700

    Store config file path in Source::ConfigFile

--- crates/task-rs/src/config/source.rs
+++ crates/task-rs/src/config/source.rs
@@ -1,10 +1,11 @@
 use serde::{Deserialize, Serialize};
 use smart_default::SmartDefault;
+use std::path::PathBuf;
 
 #[derive(Debug, SmartDefault, Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case")]
 pub enum Source {
     #[default]
-    ConfigFile,
+    ConfigFile(PathBuf),
     CommandLineArguments,
 }
