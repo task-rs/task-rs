ceda36078b7f31e55036cac353bbe8255cb2bf89
khai96_ <hvksmr1996@gmail.com>
2020-06-13 17:16:31 +0700

    Rename variable

--- crates/task-rs/src/utils/mod.rs
+++ crates/task-rs/src/utils/mod.rs
@@ -8,7 +8,7 @@ use std::{fs::File, path::PathBuf};
 pub fn deserialize_file<Output: DeserializeOwned>(filename: &PathBuf) -> Result<Output, String> {
     let file = File::open(filename)
         .map_err(|error| format!("Failed to load {}: {}", filename.to_string_lossy(), error))?;
-    let model = serde_yaml::from_reader(file)
+    let output = serde_yaml::from_reader(file)
         .map_err(|error| format!("Failed to parse {}: {}", filename.to_string_lossy(), error))?;
-    Ok(model)
+    Ok(output)
 }
