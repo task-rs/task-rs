59e0b8b32bab993376f404010834fff3ad5191ef
khai96_ <hvksmr1996@gmail.com>
2020-06-13 19:25:48 +0700

    Load data

--- crates/task-rs/src/mvc/controller/mod.rs
+++ crates/task-rs/src/mvc/controller/mod.rs
@@ -1,9 +1,34 @@
-use super::super::data::Data;
+use super::super::{config::Config, data::Data, utils::deserialize_file};
 use super::{Message, Model, UiState};
 use iced::Command;
 
+fn load_data(ui_state: &UiState) -> Data {
+    if let Some((
+        Config {
+            local_repo_location,
+            ..
+        },
+        _,
+    )) = &ui_state.config
+    {
+        match deserialize_file(&local_repo_location) {
+            Ok(data) => data,
+            Err(error) => {
+                eprintln!(
+                    "Failed to load {}: {}",
+                    local_repo_location.to_string_lossy(),
+                    error
+                );
+                Data::default()
+            }
+        }
+    } else {
+        Data::default()
+    }
+}
+
 pub fn new(ui_state: UiState) -> (Model, Command<Message>) {
-    let data = Data::default();
+    let data = load_data(&ui_state);
     let model = Model { data, ui_state };
     (model, Command::none())
 }
