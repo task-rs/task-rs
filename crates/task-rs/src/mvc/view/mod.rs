5b8f537ec16a75965a3938a09584520577fd7b83
khai96_ <hvksmr1996@gmail.com>
2020-06-14 10:22:13 +0700

    Save it for now

--- crates/task-rs/src/mvc/view/mod.rs
+++ crates/task-rs/src/mvc/view/mod.rs
@@ -4,7 +4,8 @@ use iced::*;
 pub fn view(model: &mut Model) -> Element<'_, Message> {
     let color_scheme = &model.ui_state.theme.color_scheme();
 
-    Row::new()
-        .push(Column::new().push(Text::new("TaskRs").color(color_scheme.text).size(40)))
-        .into()
+    let row =
+        Row::new().push(Column::new().push(Text::new("TaskRs").color(color_scheme.text).size(40)));
+
+    Container::new(row).into()
 }
