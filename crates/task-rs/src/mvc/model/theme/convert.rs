c1a0e03d130583b48b715bf09d8a89668cd4c485
khai96_ <hvksmr1996@gmail.com>
2020-06-14 11:39:47 +0700

    Rename method

--- crates/task-rs/src/mvc/model/theme/convert.rs
+++ crates/task-rs/src/mvc/model/theme/convert.rs
@@ -2,7 +2,7 @@ use super::super::super::super::style::{Style, DARK, LIGHT};
 use super::Theme;
 
 impl Theme {
-    pub fn color_scheme(&self) -> Style {
+    pub fn style(&self) -> Style {
         match self {
             Theme::Light => LIGHT,
             Theme::Dark => DARK,
