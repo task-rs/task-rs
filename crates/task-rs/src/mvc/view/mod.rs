7900dc1418de5fba0edfd2ae65a1aa790ff78ffa
khai96_ <hvksmr1996@gmail.com>
2020-06-14 21:22:11 +0700

    Add tag to sidebar

--- crates/task-rs/src/mvc/view/mod.rs
+++ crates/task-rs/src/mvc/view/mod.rs
@@ -4,7 +4,17 @@ use iced::*;
 pub fn view(model: &mut Model) -> Element<'_, Message> {
     let style = model.ui_state.theme.style();
 
-    let sidebar = Column::new().push(Text::new("TaskRs").size(40));
+    let sidebar = {
+        let mut sidebar = Column::new().push(Text::new("TaskRs").size(40));
+
+        for (tag_id, tag_data) in &model.data.tags {
+            sidebar = sidebar.push(Text::new(
+                tag_data.name.clone().unwrap_or_else(|| tag_id.0.clone()),
+            ));
+        }
+
+        sidebar
+    };
 
     let row = Row::new().push(sidebar);
 
