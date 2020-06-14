db53905d610701e8a0517d242a0f3847b26e05d3
khai96_ <hvksmr1996@gmail.com>
2020-06-14 21:37:09 +0700

    Move sidebar creation to its own module

--- crates/task-rs/src/mvc/view/mod.rs
+++ crates/task-rs/src/mvc/view/mod.rs
@@ -1,29 +1,15 @@
-use super::super::data::tag;
 use super::{Message, Model};
 use iced::*;
-use pipe_trait::*;
 
 pub fn view(model: &mut Model) -> Element<'_, Message> {
     let style = model.ui_state.theme.style();
 
-    let sidebar = {
-        let mut sidebar = Column::new().push(Text::new("TaskRs").size(40));
-
-        for entry in &model.data.tags {
-            sidebar = entry
-                .pipe(tag::entry::display)
-                .pipe(Text::new)
-                .pipe(|text| sidebar.push(text))
-        }
-
-        sidebar
-    };
-
-    let row = Row::new().push(sidebar);
+    let row = Row::new().push(sidebar::create(model));
 
     Container::new(row)
         .style(stylesheets::Container(style))
         .into()
 }
 
+mod sidebar;
 mod stylesheets;
---
+++ crates/task-rs/src/mvc/view/sidebar.rs
@@ -0,0 +1,17 @@
+use super::super::super::data::tag;
+use super::super::{Message, Model};
+use iced::*;
+use pipe_trait::*;
+
+pub fn create(model: &Model) -> Element<'_, Message> {
+    let mut sidebar = Column::new().push(Text::new("TaskRs").size(40));
+
+    for entry in &model.data.tags {
+        sidebar = entry
+            .pipe(tag::entry::display)
+            .pipe(Text::new)
+            .pipe(|text| sidebar.push(text))
+    }
+
+    sidebar.into()
+}
