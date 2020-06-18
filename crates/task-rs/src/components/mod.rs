4ba198aaf0a59d8910c28c7df28cbc0a1fa3b092
khai96_ <hvksmr1996@gmail.com>
2020-06-18 14:26:02 +0700

    Make it public

--- crates/task-rs/src/components/mod.rs
+++ crates/task-rs/src/components/mod.rs
@@ -3,13 +3,11 @@ pub mod sidebar;
 pub mod task_status_filter;
 pub mod theme;
 pub mod theme_switcher;
+pub mod controls;
 
 pub use header::Header;
 pub use sidebar::Sidebar;
 pub use task_status_filter::TaskStatusFilter;
 pub use theme::Theme;
 pub use theme_switcher::ThemeSwitcher;
-
-pub(crate) mod controls;
-
-pub(crate) use controls::Controls;
+pub use controls::Controls;
