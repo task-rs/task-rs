79770f196573936b0f582a928ecb0b4ce189680c
khai96_ <hvksmr1996@gmail.com>
2020-06-19 19:19:10 +0700

    fmt

--- preview/tasks/src/main.rs
+++ preview/tasks/src/main.rs
@@ -10,8 +10,10 @@ fn main() {
         tags: btreemap! {
             "abc".into() => TagData::default(),
             "def".into() => TagData::default(),
-            "ghi".into() => TagData::default().with_name("GHI Tag"),
-            "jkl".into() => TagData::default().with_description("This is a tag with id 'jkl'"),
+            "ghi".into() => TagData::default()
+                .with_name("GHI Tag"),
+            "jkl".into() => TagData::default()
+                .with_description("This is a tag with id 'jkl'"),
             "mno".into() => TagData::default()
                 .with_name("MNO Tag")
                 .with_description("This is a tag with id 'mno'"),
