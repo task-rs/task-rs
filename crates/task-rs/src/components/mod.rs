2ceb40ab299dcb24da454ede99a2699592cd99ae
khai96_ <hvksmr1996@gmail.com>
2020-06-19 09:18:33 +0700

    fmt

--- crates/task-rs/src/components/mod.rs
+++ crates/task-rs/src/components/mod.rs
@@ -1,13 +1,13 @@
+pub mod controls;
 pub mod header;
 pub mod sidebar;
 pub mod task_status_filter;
 pub mod theme;
 pub mod theme_switcher;
-pub mod controls;
 
+pub use controls::Controls;
 pub use header::Header;
 pub use sidebar::Sidebar;
 pub use task_status_filter::TaskStatusFilter;
 pub use theme::Theme;
 pub use theme_switcher::ThemeSwitcher;
-pub use controls::Controls;
