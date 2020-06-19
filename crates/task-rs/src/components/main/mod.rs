c711109bdb187caafc94fbe1c9b824490498a74e
khai96_ <hvksmr1996@gmail.com>
2020-06-19 13:04:36 +0700

    Stop pointless flattening

--- crates/task-rs/src/components/main/mod.rs
+++ crates/task-rs/src/components/main/mod.rs
@@ -1,17 +1,10 @@
-use super::super::{
-    data::Data,
-    mvc::{model, UiState},
-};
-use super::{
-    task_status_filter, Controls, Header, Sidebar, TaskStatusFilter, Theme, ThemeSwitcher,
-};
+use super::super::mvc::{model, Model};
+use super::{task_status_filter, Header, Sidebar, TaskStatusFilter, Theme, ThemeSwitcher};
 use iced::*;
 use pipe_trait::*;
 
 pub struct Main<'a, Message> {
-    pub data: &'a mut Data,
-    pub ui_state: &'a mut UiState,
-    pub controls: &'a mut Controls,
+    pub model: &'a mut Model,
     pub set_task_status_filter: fn(task_status_filter::Value) -> Message,
     pub set_dark_mode: fn(bool) -> Message,
 }
@@ -21,25 +14,25 @@ where
     Message: Clone + 'static,
 {
     fn into(self) -> Element<'a, Message> {
-        let theme = &self.ui_state.theme;
-        let task_status_filter = self.ui_state.details.task_status_filter;
+        let theme = &self.model.ui_state.theme;
+        let task_status_filter = self.model.ui_state.details.task_status_filter;
 
         Column::new()
             .push(Header {
                 task_status_filter: TaskStatusFilter {
-                    controls: &mut self.controls.task_status_filter,
+                    controls: &mut self.model.controls.task_status_filter,
                     actual_value: task_status_filter,
                     get_message: self.set_task_status_filter,
                     theme,
                 },
                 theme_switcher: ThemeSwitcher {
-                    dark_mode: self.ui_state.theme == model::Theme::Dark,
+                    dark_mode: self.model.ui_state.theme == model::Theme::Dark,
                     get_message: self.set_dark_mode,
-                    controls: &mut self.controls.theme_switcher,
+                    controls: &mut self.model.controls.theme_switcher,
                     theme,
                 },
             })
-            .push(Row::new().push(Sidebar(&self.data.tags)))
+            .push(Row::new().push(Sidebar(&self.model.data.tags)))
             .pipe(Container::new)
             .style(stylesheets::Container(theme.style()))
             .into()
--- crates/task-rs/src/mvc/view/mod.rs
+++ crates/task-rs/src/mvc/view/mod.rs
@@ -3,16 +3,8 @@ use super::{Message, Model};
 use iced::*;
 
 pub fn view(model: &mut Model) -> Element<'_, Message> {
-    let Model {
-        ref mut data,
-        ref mut ui_state,
-        ref mut controls,
-    } = model;
-
     Main {
-        data,
-        ui_state,
-        controls,
+        model,
         set_task_status_filter: Message::SetTaskStatusFilter,
         set_dark_mode: Message::SetDarkMode,
     }
