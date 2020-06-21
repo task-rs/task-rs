63ac3ae383137cdf919dcbca76208d1b55159567
khai96_ <hvksmr1996@gmail.com>
2020-06-21 13:26:12 +0700

    Add tag_list::Controls to controls::Controls

--- crates/task-rs/src/components/controls.rs
+++ crates/task-rs/src/components/controls.rs
@@ -1,4 +1,5 @@
 pub use super::tag_filter_method::Controls as TagFilterMethod;
+pub use super::tag_list::Controls as TagList;
 pub use super::task_status_filter::Controls as TaskStatusFilter;
 pub use super::theme_switcher::Controls as ThemeSwitcher;
 
@@ -7,4 +8,5 @@ pub struct Controls {
     pub(crate) task_status_filter: TaskStatusFilter,
     pub(crate) theme_switcher: ThemeSwitcher,
     pub(crate) tag_filter_method: TagFilterMethod,
+    pub(crate) tag_list: TagList,
 }
