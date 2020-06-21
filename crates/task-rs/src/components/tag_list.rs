249b97e8e6b9faa5f8d122f2baa51c67b2e5eb7c
khai96_ <hvksmr1996@gmail.com>
2020-06-21 13:48:18 +0700

    Update TagList

--- crates/task-rs/src/components/tag_list.rs
+++ crates/task-rs/src/components/tag_list.rs
@@ -12,20 +12,32 @@ where
     pub(crate) controls: &'a mut Controls,
     pub get_content: fn(&TagId) -> Element<'a, Message>,
     pub get_message: fn(&TagId) -> Message,
+    pub get_activated: fn(&TagId) -> bool,
     pub theme: Theme,
 }
 
 impl<'a, Theme, Message> Into<Element<'a, Message>> for TagList<'a, Theme, Message>
 where
     Message: Clone + 'a,
-    Theme: style::Theme,
+    Theme: style::Theme + Copy,
 {
     fn into(self) -> Element<'a, Message> {
+        let TagList {
+            controls,
+            get_activated,
+            get_message,
+            get_content,
+            theme,
+        } = self;
+
         ButtonList {
-            controls: self.controls,
-            get_content: self.get_content,
-            get_message: self.get_message,
-            theme: self.theme,
+            controls,
+            get_content,
+            get_message,
+            get_style: |id| style::BinaryStateButton {
+                activated: get_activated(id),
+                style: theme.style(),
+            },
         }
         .pipe(Into::<Column<'a, Message>>::into)
         .into()
