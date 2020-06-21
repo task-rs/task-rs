d0750fd0beb598971ba4f65cdb8d7b2292730030
khai96_ <hvksmr1996@gmail.com>
2020-06-21 13:39:30 +0700

    Implement styling and messaging for ButtonList

--- crates/task-rs/src/components/button_list/mod.rs
+++ crates/task-rs/src/components/button_list/mod.rs
@@ -2,35 +2,36 @@ pub mod controls;
 
 pub use controls::Controls;
 
-use super::super::style;
 use iced::*;
 
-pub struct ButtonList<'a, Key, Value, Theme, Message>
+pub struct ButtonList<'a, Key, Value, Message, ButtonStyle>
 where
-    Theme: style::Theme,
     Key: Ord,
+    ButtonStyle: button::StyleSheet,
 {
     pub(crate) controls: &'a mut Controls<Key>,
     pub get_content: fn(&Key) -> Value,
     pub get_message: fn(&Key) -> Message,
-    pub theme: Theme,
+    pub get_style: fn(&Key) -> ButtonStyle,
 }
 
 macro_rules! impl_into {
     ($container:ident) => {
-        impl<'a, Key, Theme, Message> Into<$container<'a, Message>>
-            for ButtonList<'a, Key, Element<'a, Message>, Theme, Message>
+        impl<'a, Key, Message, ButtonStyle> Into<$container<'a, Message>>
+            for ButtonList<'a, Key, Element<'a, Message>, Message, ButtonStyle>
         where
             Key: Ord + Clone + 'a,
-            Theme: style::Theme,
             Message: Clone + 'a,
+            ButtonStyle: button::StyleSheet + 'static,
         {
             fn into(self) -> $container<'a, Message> {
                 let mut container = $container::new();
 
                 for (key, state) in self.controls.0.iter_mut() {
                     let value = (self.get_content)(key);
-                    let button = Button::new(state, value);
+                    let button = Button::new(state, value)
+                        .on_press((self.get_message)(key))
+                        .style((self.get_style)(key));
                     container = container.push(button);
                 }
 
