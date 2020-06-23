6f0e549e7f49ad0176128bd939664834fb1c406c
khai96_ <hvksmr1996@gmail.com>
2020-06-23 16:20:57 +0700

    Create some tests for Index

--- crates/task-rs/src/utils/indexed_map/index.rs
+++ crates/task-rs/src/utils/indexed_map/index.rs
@@ -7,10 +7,22 @@ impl From<u32> for Index {
     }
 }
 
+#[test]
+fn test_from_num() {
+    assert_eq!(Index::from(123), Index(123));
+}
+
 impl Into<u32> for Index {
     fn into(self) -> u32 {
         self.0
     }
+}
+
+#[test]
+fn test_into_num() {
+    let index = Index::from(123);
+    let num: u32 = index.into();
+    assert_eq!(num, 123);
 }
 
 impl AsRef<u32> for Index {
@@ -19,8 +31,25 @@ impl AsRef<u32> for Index {
     }
 }
 
+#[test]
+fn test_as_ref() {
+    let index = Index::from(123);
+    assert_eq!(index.as_ref(), &123);
+}
+
 impl AsMut<u32> for Index {
     fn as_mut(&mut self) -> &mut u32 {
         &mut self.0
     }
+}
+
+#[test]
+fn test_as_mut() {
+    let mut index = Index::from(123);
+    assert_eq!(index.0, 123);
+    {
+        let index_ref = index.as_mut();
+        *index_ref += 321;
+    }
+    assert_eq!(index.0, 123 + 321);
 }
