86dfb8097f3b6fcbabbbd1a2c5da6dbd904f29ea
khai96_ <hvksmr1996@gmail.com>
2020-06-24 10:54:48 +0700

    Change CheckboxButton methods

--- crates/task-rs/src/components/checkbox_button.rs
+++ crates/task-rs/src/components/checkbox_button.rs
@@ -1,29 +1,19 @@
 use iced::*;
 
 pub struct CheckboxButton<'a, Message> {
+    pub state: &'a mut button::State,
     pub checked: bool,
     pub content: Element<'a, Message>,
 }
 
-impl<'a, Message> CheckboxButton<'a, Message> {
-    pub fn from_widget(checked: bool, content: impl Into<Element<'a, Message>>) -> Self {
-        CheckboxButton {
-            checked,
-            content: content.into(),
-        }
-    }
-
-    pub fn from_text(checked: bool, content: impl Into<String>) -> Self {
-        Self::from_widget(checked, Text::new(content))
-    }
-
-    pub fn into_button(self, state: &'a mut button::State) -> Button<Message>
-    where
-        Message: 'a,
-    {
+impl<'a, Message> Into<Button<'a, Message>> for CheckboxButton<'a, Message>
+where
+    Message: 'a,
+{
+    fn into(self) -> Button<'a, Message> {
         let content = Row::new()
             .push(Text::new(if self.checked { "✓" } else { " " }))
             .push(self.content);
-        Button::new(state, content)
+        Button::new(self.state, content)
     }
 }
