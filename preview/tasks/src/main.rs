48b0a03dc9b275794da71a847f5bd0689ead5a58
khai96_ <hvksmr1996@gmail.com>
2020-06-26 20:27:20 +0700

    refactor: Move construction of TagMap out

--- preview/tasks/src/main.rs
+++ preview/tasks/src/main.rs
@@ -6,19 +6,21 @@ use task_rs::{
 };
 
 fn main() {
+    let tags = btreemap! {
+        "abc".into() => TagData::default(),
+        "def".into() => TagData::default(),
+        "ghi".into() => TagData::default()
+            .with_name("GHI Tag"),
+        "jkl".into() => TagData::default()
+            .with_description("This is a tag with id 'jkl'"),
+        "mno".into() => TagData::default()
+            .with_name("MNO Tag")
+            .with_description("This is a tag with id 'mno'"),
+    }
+    .into();
+
     let data = Data {
-        tags: btreemap! {
-            "abc".into() => TagData::default(),
-            "def".into() => TagData::default(),
-            "ghi".into() => TagData::default()
-                .with_name("GHI Tag"),
-            "jkl".into() => TagData::default()
-                .with_description("This is a tag with id 'jkl'"),
-            "mno".into() => TagData::default()
-                .with_name("MNO Tag")
-                .with_description("This is a tag with id 'mno'"),
-        }
-        .into(),
+        tags,
         ..Data::default()
     };
 
