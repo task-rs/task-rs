2f4b25824ecd42c3987748115df744a01f8a8cdb
khai96_ <hvksmr1996@gmail.com>
2020-06-27 12:28:55 +0700

    Move data from main.rs to data.yaml

--- preview/tasks/src/main.rs
+++ preview/tasks/src/main.rs
@@ -1,51 +1,14 @@
-use maplit::btreemap;
+use pipe_trait::*;
 use task_rs::{
-    data::{Data, Status, TagData, TagId, Task},
     iced::{Application, Settings},
     mvc::Model,
+    serde_yaml,
 };
 
-fn create_task(
-    completed: bool,
-    summary: impl ToString,
-    details: impl ToString,
-    sub: impl IntoIterator<Item = Task>,
-    tags: impl IntoIterator<Item = TagId>,
-) -> Task {
-    Task {
-        status: if completed {
-            Status::Completed
-        } else {
-            Status::Active
-        },
-        summary: summary.to_string(),
-        details: details.to_string(),
-        sub: sub.into_iter().collect(),
-        tags: tags.into_iter().collect(),
-    }
-}
-
 fn main() {
-    let tags = btreemap! {
-        "abc".into() => TagData::default(),
-        "def".into() => TagData::default(),
-        "ghi".into() => TagData::default()
-            .with_name("GHI Tag"),
-        "jkl".into() => TagData::default()
-            .with_description("This is a tag with id 'jkl'"),
-        "mno".into() => TagData::default()
-            .with_name("MNO Tag")
-            .with_description("This is a tag with id 'mno'"),
-    }
-    .into();
-
-    let tasks = vec![create_task(false, "first task", "", vec![], vec![])];
-
-    let data = Data {
-        tags,
-        tasks,
-        ..Data::default()
-    };
+    let data = include_str!("./assets/data.yaml")
+        .pipe(serde_yaml::from_str)
+        .unwrap();
 
     let settings = Settings {
         flags: Model {
