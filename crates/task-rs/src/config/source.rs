8103fb979f72fcda462b2606a9a9599dd5210243
khai96_ <hvksmr1996@gmail.com>
2020-07-01 13:28:56 +0700

    Make config clonable

--- crates/task-rs/src/config/source.rs
+++ crates/task-rs/src/config/source.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use smart_default::SmartDefault;
 use std::path::PathBuf;
 
-#[derive(Debug, SmartDefault, Serialize, Deserialize)]
+#[derive(Debug, SmartDefault, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "kebab-case")]
 pub enum Source {
     #[default]
