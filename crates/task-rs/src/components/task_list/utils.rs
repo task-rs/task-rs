0931f0ad929bb6516a08fd6a7730be717f8f6c99
khai96_ <hvksmr1996@gmail.com>
2020-07-08 10:54:31 +0700

    Test extend_task_item_list

--- crates/task-rs/src/components/task_list/utils.rs
+++ crates/task-rs/src/components/task_list/utils.rs
@@ -8,3 +8,28 @@ pub fn extend_task_item_list(target: &mut Vec<TaskItem>, tasks: &[Task], address
         extend_task_item_list(target, &task.sub, &prefix());
     }
 }
+
+#[test]
+fn test_extend_task_item_list() {
+    use pipe_trait::*;
+    let tasks: Vec<_> = include_str!("./fixtures/task-items.yaml")
+        .pipe(serde_yaml::from_str)
+        .unwrap();
+    let mut task_items = Vec::new();
+
+    extend_task_item_list(&mut task_items, &tasks, &[]);
+
+    let actual: Vec<_> = task_items
+        .iter()
+        .map(|item| (item.task_address.as_slice(), item.task_summary.as_str()))
+        .collect();
+    let expected: Vec<(&[usize], &str)> = vec![
+        (&[0], "first task"),
+        (&[1], "task with a sub"),
+        (&[1, 0], "first sub task"),
+        (&[2], "task with 2 subs"),
+        (&[2, 0], "completed sub task"),
+        (&[2, 1], "active sub task"),
+    ];
+    assert_eq!(actual, expected);
+}
